32f90391eb5cb5314288148af2888608dd3f9543
StrawbWine <severin.hanevik@gmail.com>
2022-06-22 20:03:00 +0200

    🔥Clean up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { useState } from "react"
 import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
 import './App.css';
-import useApi from "./hooks/useApi";
-import FoodContainer from "./components/FoodContainer/FoodContainer";
-import CreateFoodForm from "./components/CreateFoodForm/CreateFoodForm";
-import CreateFoodPortionForm from "./components/CreateFoodPortionForm/CreateFoodPortionForm";
 import Navbar from "./components/Navbar/Navbar";
 import FoodPage from "./components/FoodPage/FoodPage";
 import FoodPortionPage from "./components/FoodPortionPage/FoodPortionPage";
 
 function App() {
-  // const API_URL = process.env.REACT_APP_API_URL
-
-  // const [foodData, getFoodData, setFoodData] = useApi(`${API_URL}/food`)
-  // const [activeFood, setActiveFood] = useState(null)
-
   return (
     <Router>
       <div className="App">
