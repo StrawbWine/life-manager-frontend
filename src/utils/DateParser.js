7da9e8021971adcf4031f61204609895dd95fee4
StrawbWine <severin.hanevik@gmail.com>
2022-07-17 07:01:32 +0200

    ✔️🐛Fix bug in DateParser

--- src/utils/DateParser.js
+++ src/utils/DateParser.js
@@ -1,6 +1,6 @@
 const DateToString = date => {
   const year = date.getFullYear().toString()
-  const month = (date.getMonth() + 1 > 9 ? "" : "0") + (date.getMonth() + 1).toString()
+  const month = (date.getMonth() + 1 > 9 ? "" : "0") + (date.getMonth()).toString()
   const dayOfMonth = (date.getDate() > 9 ? "" : "0") + date.getDate().toString()
 
   return `${year}-${month}-${dayOfMonth}`
