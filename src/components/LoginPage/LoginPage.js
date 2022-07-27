e71194d220696896536b3e03069914618283f1e8
StrawbWine <severin.hanevik@gmail.com>
2022-07-27 09:15:26 +0200

    Style login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import API_URL from "../../constants"
 import axios from "axios"
+import styles from "./LoginPage.module.css"
 
 const loginUser = async credentials => {
   const response = await axios.post(`${API_URL}/authenticate`, credentials)
@@ -23,19 +24,19 @@ export default function Login({ setToken }) {
   }
 
   return (
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
+    <div className={styles.LoginPage}>
+      <h1 className={styles.LoginTitle}>Please Log In</h1>
+      <form className={styles.LoginForm} onSubmit={handleSubmit}>
         <label>
-          <p>Username</p>
+          <p className={styles.LoginLabel}>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)} />
         </label>
         <label>
-          <p>Password</p>
+          <p className={styles.LoginLabel}>Password</p>
           <input type="password" onChange={e => setPassword(e.target.value)} />
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button className={styles.LoginButton} type="submit" >Submit</button>
         </div>
       </form>
     </div>
